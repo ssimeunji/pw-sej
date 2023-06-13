52f340ce5eab36ec6efedf6620986c3e064269d1
simeunji <ssimeunji@wrapcore.co.kr>
2023-06-13 13:58:23 +0900

    test

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-self.addEventListener('push', function (e) {
+self.addEventListener('fetch', function (e) {
 	var options = {
 		body: 'This notification was generated from a push!',
 		icon: 'images/example.png',
